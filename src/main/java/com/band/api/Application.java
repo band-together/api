9363dd1299dc6d0c04889b3278e000f7de5bd640
TheLastKiwi <TheLastKiwi@users.noreply.github.com>
2019-04-13 17:23:07 -0700

    Adds dependencies (#6)
    * Adds graphql dependencies, removes security login page
    
    * idk
    
    * Add newline at end of file
    
    * spaces instead of tabs

--- src/main/java/com/band/api/Application.java
+++ src/main/java/com/band/api/Application.java
@@ -2,10 +2,11 @@ package com.band.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.band.api.repository")
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
