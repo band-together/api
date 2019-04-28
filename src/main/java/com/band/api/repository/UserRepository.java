2c551beff9655970ec7ed9c02b36d8532f21ba0e
Matt Davis <mdavis2599@gmail.com>
2019-04-27 18:12:29 -0700

    Tests added

--- src/main/java/com/band/api/repository/UserRepository.java
+++ src/main/java/com/band/api/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.band.api.repository;
 
 import com.band.api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     User findTopByUsername(String username);
 
     User findTopByEmailSearch(String email);
